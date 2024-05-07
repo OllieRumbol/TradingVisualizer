873873323188c87695c849565f98d79c58eecc71
Oliver Bourne <otgb97@gmail.com>
2024-05-07 22:24:27 +0100

    add top banner

--- trading-visualizer/src/pages/app/App.tsx
+++ trading-visualizer/src/pages/app/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
+import Banner from "components/banner/Banner";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Banner />
     </div>
   );
 }
